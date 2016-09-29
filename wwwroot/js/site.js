4e8cf29c70455989ddc5d529dce235db01880307
Cesar Lopez <cesarl@outlook.com>
2016-09-29 09:44:17 -0500

    Introducing jQuery

---
+++ wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿// site.js
+(function () {
+
+    var ele = $("#username");
+    ele.text("Cesar Lerma");
+
+    var main = $("#main");
+
+    main.hover(
+        function () {
+            main.css("background-color","#888");
+        },
+        function () {
+            main.css("background-color", "");
+        }
+    );
+
+    var menuItems = $("ul.menu li a");
+    menuItems.click(function () {
+
+        var me = $(this);
+
+        alert("Hello: " + me.text());
+
+    });
+
+})();
