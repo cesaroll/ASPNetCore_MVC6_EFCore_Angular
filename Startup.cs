b31ac3adeae24645370c23d5716f324de25bdc92
Cesar Lopez <cesarl@outlook.com>
2016-10-22 22:59:28 -0500

    Implementing Coordinates Service using Bing Maps

--- Startup.cs
+++ Startup.cs
@@ -49,6 +49,8 @@ namespace TheWorld
 
             services.AddScoped<IWorldRepository, WorldRepository>();
 
+            services.AddTransient<GeoCoordsService>();
+
             services.AddTransient<WorldContextSeedData>();
 
             services.AddLogging();
