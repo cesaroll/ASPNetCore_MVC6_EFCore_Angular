19f07ee1dae7fd7f0cc69e941596469c0383d519
Cesar Lopez <cesarl@outlook.com>
2016-10-24 21:51:28 -0500

    Implement Login

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheWorld.Models;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers
 {
     public class AuthController : Controller
     {
+        private SignInManager<WorldUser> _signInManager;
+
+        public AuthController(SignInManager<WorldUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         public IActionResult Login()
         {
 
@@ -19,5 +29,35 @@ namespace TheWorld.Controllers
             
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel vm, string returnUrl)
+        {
+            if(ModelState.IsValid)
+            {
+                var signInResult = await _signInManager.PasswordSignInAsync(vm.Username,
+                                                                        vm.Password,
+                                                                        true,
+                                                                        false);
+                if(signInResult.Succeeded)
+                {
+                    if(string.IsNullOrWhiteSpace(returnUrl))
+                    {
+                        return RedirectToAction("Trips", "App");
+                    }
+                    else
+                    {
+                        return Redirect(returnUrl);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or passwrd incorrect");
+                }
+            }
+
+            return View();
+        }
+
     }
 }
