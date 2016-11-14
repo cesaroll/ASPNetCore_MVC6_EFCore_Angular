04456dff4ef4f3e956b5ca2df73a2fd01d537fbf
Cesar Lopez <cesarl@outlook.com>
2016-11-14 13:35:06 -0600

    Adding Routing

--- wwwroot/js/app-trips.js
+++ wwwroot/js/app-trips.js
@@ -3,6 +3,17 @@
     "use strict";
 
     //Creating the Module
-    angular.module("app-trips", ["simpleControlls"])
+    angular.module("app-trips", ["ngRoute", "simpleControlls"])
+        .config(function ($routeProvider) {
+
+            $routeProvider.when("/", {
+                controller: "tripsController",
+                controllerAs: "vm",
+                templateUrl: "/views/tripsView.html"
+            });
+
+            $routeProvider.otherwise({redirectTo: "/"}); //Any other not provided route would be redirect to first main route
+
+        });
 
 })();
