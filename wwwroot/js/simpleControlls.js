bc6d8f158393b9408534ef77080b818a1390a869
Cesar Lopez <cesarl@outlook.com>
2016-11-14 12:21:29 -0600

    Creating a Directive

---
+++ wwwroot/js/simpleControlls.js
@@ -0,0 +1,19 @@
+﻿// simpleControlls.js
+(function () {
+
+    "use strict";
+
+    angular.module("simpleControlls", [])
+        .directive("waitCursor", waitCursor);
+
+    function waitCursor() {
+        return {
+            templateUrl: "/views/waitCursor.html",
+            restrict: "E",
+            scope: {
+                show: "=displayWhen"
+            }
+        };
+    }
+
+})();
